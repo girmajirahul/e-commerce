91093952edf2ce00f4ecbbaf533113fe3d569ae9
RSG-developer <girmajirahul2@gmail.com>
2025-06-27 19:25:21 +0530

    Signup

--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -3,63 +3,63 @@ import Alert from './Alert';
 import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   const [showSignup, setShowSignup] = useState(false);
-  const [formData,setformData]=useState({
-    username:'',
-    fullname:'',
-    email:'',
-    password:'',
+  const [formData, setformData] = useState({
+    username: '',
+    fullname: '',
+    email: '',
+    password: '',
   });
-  const [alert , setAlert]=useState({message:'' , type:''});
-  const handleChange=(e)=>{
+  const [alert, setAlert] = useState({ message: '', type: '' });
+  const handleChange = (e) => {
     setformData(prev => ({ ...prev, [e.target.name]: e.target.value }));
   }
 
-  const handleSubmmit= async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const endpoint=showSignup ? '/signup':'/login';
-    try{
-        const response =await fetch(`http://localhost:3000${endpoint}`,{
-          method:"POST",
-          headers:{
-            "Content-Type": "application/json"
-          },
-          body:JSON.stringify(formData),
-        });
+    const endpoint = showSignup ? '/signup' : '/login';
+    try {
+      const response = await fetch(`http://localhost:3000/user${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData),
+      });
 
-        const result =await response.json()
-        // alert(result.message || "Success");
-        if(response.ok){
-          if(showSignup){
-            setAlert({message:result.message || 'Signup Successful !' , type:'success'})
-            setShowSignup(false)
-          }
-          else{
-            localStorage.setItem('user',JSON.stringify(result.user));
-            setTimeout(()=>{
-              navigate('/');
-            },1000)
-            setAlert({message:result.message || 'Login Successful !' , type:'success'})
-          }
-          // setAlert({message:result.message || 'Success !' , type:'success'})
+      const result = await response.json()
+      // alert(result.message || "Success");
+      if (response.ok) {
+        if (showSignup) {
+          setAlert({ message: result.message || 'Signup Successful !', type: 'success' })
+          setShowSignup(false)
         }
-        else{
-          setAlert({message:result.message || 'Failed  !' , type:'error'})
+        else {
+          localStorage.setItem('user', JSON.stringify(result.user));
+          setTimeout(() => {
+            navigate('/');
+          }, 1000)
+          setAlert({ message: result.message || 'Login Successful !', type: 'success' })
         }
+        // setAlert({message:result.message || 'Success !' , type:'success'})
+      }
+      else {
+        setAlert({ message: result.message || 'Failed  !', type: 'error' })
+      }
 
-    }catch(err){
-      console.log("error submit form ",err)
+    } catch (err) {
+      console.log("error submit form ", err)
       setAlert({ message: 'Something Went Wrong!', type: 'error' })
     }
 
   }
-  const toggleForm = () => setShowSignup((prev) => !prev);
+  
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md bg-white p-8 shadow-md rounded-lg">
-      {alert.message && (
+        {alert.message && (
           <Alert
             message={alert.message}
             type={alert.type}
@@ -70,8 +70,8 @@ export default function Signup() {
           <h3>{showSignup ? 'Create A New Account' : 'Already a member? Login'}</h3>
         </div>
 
-        <form method="post" onSubmit={handleSubmmit} className="space-y-4">
-          {showSignup && (
+        <form method="post" onSubmit={handleSubmit} className="space -y-4">
+          {showSignup ? (
             <>
               <div className="flex flex-col">
                 <label htmlFor="username" className="mb-1 font-medium">Username</label>
@@ -98,56 +98,103 @@ export default function Signup() {
                   className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="mb-1 font-medium">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter email"
+                  className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label htmlFor="password" className="mb-1 font-medium">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter password"
+                  className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+
+              <div className="pt-4">
+                <button
+                  type="submit"
+                  className="w-full bg-blue-900 hover:bg-blue-800 text-white font-semibold py-2 px-4 rounded-full transition duration-300"
+                >
+                  {showSignup ? 'Sign Up Now' : 'Login'}
+                </button>
+              </div>
+              <div className="text-center mt-3">
+                <h4>Already have account 
+                <button
+                  type="button"
+                  onClick={() => setShowSignup(false)}
+                  className="text-blue-800 font-semibold ml-1 hover:underline cursor-pointer"
+                >
+                  Login
+                </button>
+                </h4>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="mb-1 font-medium">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter email"
+                  className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+
+              <div className="flex flex-col mt-2">
+                <label htmlFor="password" className="mb-1 font-medium">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Enter password"
+                  className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+
+              <div className="pt-4">
+                <button
+                  type="submit"
+                  className="w-full bg-blue-900 hover:bg-blue-800 text-white font-semibold py-2 px-4 rounded-full transition duration-300"
+                >
+                  Login
+                </button>
+
+              </div>
+              <div className='text-center mt-3'>
+                <h4>
+                  Don't have an account
+                  <button
+                    type="button"
+                    onClick={()=>setShowSignup(true)}
+                    className="text-blue-800 font-semibold hover:underline cursor-pointer ml-1"
+                  >
+                    Sing up
+                  </button>
+                </h4>
+              </div>
             </>
           )}
-
-          <div className="flex flex-col">
-            <label htmlFor="email" className="mb-1 font-medium">Email</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter email"
-              className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label htmlFor="password" className="mb-1 font-medium">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Enter password"
-              className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div className="pt-4">
-            <button
-              type="submit"
-              className="w-full bg-blue-900 hover:bg-blue-800 text-white font-semibold py-2 px-4 rounded-full transition duration-300"
-            >
-              {showSignup ? 'Sign Up Now' : 'Login'}
-            </button>
-          </div>
-
-          <div className="text-center">
-            <h4>
-              {showSignup ? 'Already have an account?' : "Don't have an account?"}{' '}
-              <button
-                type="button"
-                onClick={toggleForm}
-                className="text-blue-800 font-semibold underline ml-1"
-              >
-                {showSignup ? 'Login' : 'Sign up'}
-              </button>
-            </h4>
-          </div>
         </form>
       </div>
     </div>
